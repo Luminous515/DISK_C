dc76928fdf6e12b0e56d5b95faba464a53e22b0c
Harold <harold914@outlook.com>
2021-02-17 17:30:46 +0800

    1.2

--- test/INCLUDE/data.h
+++ test/INCLUDE/data.h
@@ -1,2 +1,15 @@
 #ifndef _DATA_H_
 #define _DATA_H_
+
+typedef struct userinfo
+{
+    char username[15];
+    char password[15];
+}USER;
+
+typedef struct admininfo
+{
+    char username[15];
+    char password[15];
+};
+
